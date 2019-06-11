b2aab79474398ba10cd4de4e2e0c862055c124c2
zhvakinde <49557126+zhvakinde@users.noreply.github.com>
2019-06-11 15:20:02 +0300

    Update 4.sh

--- dz5/4.sh
+++ dz5/4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ionice -c 1 dd if=/dev/urandom of=test1.img bs=1M count=20 &
-ionice -c 3 dd if=/dev/urandom of=test2.img bs=1M count=20 &
+time ionice -c 1 dd if=/dev/urandom of=test1.img bs=1M count=20 &
+time ionice -c 3 dd if=/dev/urandom of=test3.img bs=1M count=20 &
 
 wait
