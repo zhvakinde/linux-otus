5cfbc28da6275233b3853213ff52f4fdf1e4a41a
zhvakinde <49557126+zhvakinde@users.noreply.github.com>
2019-06-19 15:59:06 +0300

    Update repo.sh

--- dz6/repo.sh
+++ dz6/repo.sh
@@ -5,4 +5,5 @@ cp /vagrant/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm /usr/share/nginx/html/repo/
 wget http://www.percona.com/downloads/percona-release/redhat/0.1-6/percona-release-0.1-6.noarch.rpm -O /usr/share/nginx/html/repo/percona-release-0.1-6.noarch.rpm
 createrepo /usr/share/nginx/html/repo/
 sed -i 11i\ 'autoindex on;' /etc/nginx/conf.d/default.conf
-sudo -i systemctl start nginx
+systemctl enable nginx
+systemctl start nginx
