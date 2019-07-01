fb7f0baf6ff2e09032c45b146f6c1aace24bc553
zhvakinde <49557126+zhvakinde@users.noreply.github.com>
2019-07-01 11:17:29 +0300

    Update provision.sh

--- dz10/provision.sh
+++ dz10/provision.sh
@@ -7,5 +7,5 @@ systemctl start docker
 docker build /vagrant/ -t zhvakinde/dz10_nginx:v1
 docker run -d -p 8080:80  zhvakinde/dz10_nginx:v1
 # Дополнительное задание
-curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 $ chmod +x /usr/local/bin/docker-compose
