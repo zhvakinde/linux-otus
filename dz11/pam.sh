86ba9e32815b2ea15c16a3f82f4662556ab31a69
zhvakinde <49557126+zhvakinde@users.noreply.github.com>
2019-08-02 12:35:17 +0300

    Update pam.sh

--- dz11/pam.sh
+++ dz11/pam.sh
@@ -1,15 +1,15 @@
-if groups $PAM_USER | grep &>/dev/null '\b{{ admin }}\b'; then
-    echo "You are in the {{ admin }} group."
-    exit 0
+#!/usr/bin/env bash
+
+# Проверяем принадлежность пользователя к группе admin
+if groups $PAM_USER | grep -c admin ; then
+       exit 0
     else
-    echo "You are not in the {{ admin }} group."
 fi
 # Проверяем день. Если 0, то рабочий день.
 # Воспользуемся сервисом https://isdayoff.ru/
-# Если сервис недоступен, то никого не пускать, кроме группы {{ admin }}.
+# Если сервис недоступен, то никого не пускать, кроме группы admin.
 if [[ $(curl -s https://isdayoff.ru/$(date +%y%m%d)) -eq 0 ]]; then
     exit 0
 fi
 # Отказ по-умочанию.
-echo "Login denied. Today is a day off! Probably..."
 exit 1
