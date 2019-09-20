bb2b463150ed9f4b0931dbfc5727681d927de88a
zhvakinde <49557126+zhvakinde@users.noreply.github.com>
2019-09-20 11:15:42 +0300

    Update parse.sh

--- dz4/parse.sh
+++ dz4/parse.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 LOCK=/var/tmp/lockfile
 
-if [-f $LOCK]
+if [ -f $LOCK ]
 then
         echo "File is busy"
         exit 1
 else
         touch $LOCK
         trap 'rm -f $LOCK; exit $?' INT TERM EXIT
+
+
+rm -f $LOCK
+        trap - INT TERM EXIT
+fi
