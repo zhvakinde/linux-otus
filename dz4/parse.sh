a2e3d97abb0fc260d7f362f616a96a64231dfe5f
zhvakinde <49557126+zhvakinde@users.noreply.github.com>
2019-09-26 09:24:19 +0300

    Update parse.sh

--- dz4/parse.sh
+++ dz4/parse.sh
@@ -17,10 +17,10 @@ fi
 analyse() {
         FTARGETFILE=$1
         FRESULTFILE=$2
-        ASTART=$(cat /var/log/cron | grep parse | sort | tail -1 | awk '{print $3}')
-        AFINISH=$(date '+%H:%M:%S')
+        START=`head -n 1 $1 |awk '{print $4}'`
+        FINISH=`tail -n 1 $1 |awk '{print $4}'`
         echo "---" > $2
-        echo "Log has been analyzed from $ASTART to $AFINISH" >> $2
+        echo "Log has been analyzed from $START to $FINISH" >> $2
         echo "---" >> $2
         echo "The list of addresses with the most requests to the server (top $x req-IP pairs)" >> $2
         cat $1 |awk '{print $1}' |sort |uniq -c |sort -rn| tail -$x >> $2
@@ -28,16 +28,18 @@ analyse() {
         echo "The list of server resources with the most requests from the clients (top $y req-res pairs)" >> $2
         cat $1 |awk '{print $7}' |sort |uniq -c |sort -rn| tail -$y >> $2
         echo "---" >> $2
-        echo "Total number of errors (status codes 4xx and 5xx, number-code pairs)" >> $2
+        echo "Total number of errors " >> $2
         cat $1 |awk '{print $9}' |grep -E "[4-5]{1}[0-9][0-9]" |sort |uniq -c |sort -rn >> $2
         echo "---" >> $2
-        echo "The list of status codes with their total number (number-code pairs)" >> $2
+        echo "The list of status codes with their total number " >> $2
         cat $1 |awk '{print $9}' |sort |uniq -c |sort -rn >> $2
         echo "---" >> $2
 }
 analyse $WORKFILE $RESULTFILE
 cat $RESULTFILE | mail -s "Message fron NGINX parser" admin@test.ru
-cp $RESULTFILE /vagrant/log_parser_stat_$ASTART_$AFINISH
+
+TIME=$(cat /var/log/cron | grep parse | sort | tail -1 | awk '{print $3}')
+cp $RESULTFILE /vagrant/log_parser_stat_TIME
 rm -f $RESULTFILE
 
 rm -f $LOCK
