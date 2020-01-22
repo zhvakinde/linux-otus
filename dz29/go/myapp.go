8d97d9196dd9dec3cd059b3cf0f9649292fbb4d6
zhvakinde <49557126+zhvakinde@users.noreply.github.com>
2020-01-22 11:14:05 +0300

    Create myapp.go

---
+++ dz29/go/myapp.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "GO working here!")
+	})
+
+	http.ListenAndServe(":9001", nil)
